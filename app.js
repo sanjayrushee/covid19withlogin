1c4ba648e6e353df253d1b88cbe95217b3ed79ea
sanjayrushee <rashmikafan05g@gmail.com>
2023-12-09 08:13:46 +0000

    runable

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const databasePath = path.join(__dirname, "covid19IndiaPortal.db");
+
+const app = express();
+
+app.use(express.json());
+
+let database = null;
+
+const initializeDbAndServer = async () => {
+  try {
+    database = await open({
+      filename: databasePath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () =>
+      console.log("Server Running at http://localhost:3000/")
+    );
+  } catch (error) {
+    console.log(`DB Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+initializeDbAndServer();
+
+app.post("/login/", async (request, response) => {
+  const { username, password } = request.body;
+  const checkuser = `
+SELECT
+*
+FROM
+user
+WHERE
+username = '${username}';`;
+  const user = await database.get(checkuser);
+  if (user === undefined) {
+    response.status(400);
+    response.send("Invalid user");
+  } else {
+    const passwordCheck = await bcrypt.compare(password, user.password);
+    if (passwordCheck === true) {
+      const payload = {
+        username: username,
+      };
+      const jwtToken = jwt.sign(payload, "MY_SECRET_TOKEN");
+      response.send({ jwtToken });
+    } else {
+      response.status(400);
+      response.send("Invalid password");
+    }
+  }
+});
